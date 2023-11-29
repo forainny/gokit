8214333e02fcea4c9cbd86aa30c0f099646717d5
y <459419708@qq.com>
2023-11-30 01:37:07 +0800

    add s3 tests

--- aws/s3/s3_test.go
+++ aws/s3/s3_test.go
@@ -4,9 +4,18 @@ import "testing"
 
 func TestPresignPutObject(t *testing.T) {
 	s3Client := NewS3Client("bucket")
-	req, err := s3Client.PresignPutObject("path/to/filename.txt")
+	presign, err := s3Client.PresignPutObject("path/to/filename.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(req.URL)
+	t.Log(presign.URL)
+}
+
+func TestPutObject(t *testing.T) {
+	s3Client := NewS3Client("bucket")
+	obj, err := s3Client.PutObject("path/to/filename.txt", []byte("hello world"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(obj.Key, obj.ETag)
 }
